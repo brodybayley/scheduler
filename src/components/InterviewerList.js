49365b2cbb5bbd710f329c0f66d6da331e585412
brody bayley <brodybayley@brodys-MacBook-Air.local>
2021-03-16 14:49:25 -0700

    Add interviewerlist component, stories and styles

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import 'components/InterviewerList.scss';
+import InterviewerListItem from './InterviewerListItem';
+
+export default function InterviewerList(props) {
+  let interviewerArray = props.interviewers.map((interviewer) =>
+    <InterviewerListItem
+      key={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected={interviewer.id === props.interviewer}
+      setInterviewer={props.setInterviewer}
+    />)
+  return (
+    <ul>
+      {interviewerArray}
+    </ul>
+  )
+}
