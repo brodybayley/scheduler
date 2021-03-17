00528a16282837d16ca49f6adb964674441e7275
brody bayley <brodybayley@brodys-MacBook-Air.local>
2021-03-16 20:30:49 -0700

    Add appointment form stories and components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,12 @@
-import React from "react";
-import "components/Appointment/styles.scss";
+// import React from "react";
+// import "components/Appointment/styles.scss";
+// import Header from './Header';
+// import Show from './Show';
+// import Empty from './Empty';
 
-export default function Appointment(props) {
+// export default function Appointment(props) {
 
-  return (
-    <article className="appointment"></article>
-  )
-}
+//   return (
+//     <article className="appointment"></article>
+//   )
+// }
--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -123,14 +124,14 @@ storiesOf("InterviewerList", module)
   .add("Initial", () => (
     <InterviewerList
       interviewers={interviewers}
-      setInterviewer={action("setInterviewer")}
+      onChange={action("setInterviewer")}
     />
   ))
   .add("Preselected", () => (
     <InterviewerList
       interviewers={interviewers}
-      interviewer={3}
-      setInterviewer={action("setInterviewer")}
+      value={3}
+      onChange={action("setInterviewer")}
     />
   ));
 
@@ -162,5 +163,21 @@ storiesOf("Appointment", module)
     <Error
       message="Could not delete appointment"
       onClose={action("onClose")}
+    />
+  ))
+  .add("Edit", () => (
+    <Form
+      name="Lydia Miller-Jones"
+      interviewers={interviewers}
+      interviewer={2}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  .add("Create", () => (
+    <Form
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
     />
   ))
