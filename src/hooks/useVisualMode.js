e6497a48b9752511dcf1fa105cbe851f70eeaa0a
brody bayley <brodybayley@brodys-MacBook-Air.local>
2021-03-18 15:52:37 -0700

    Add useVisualMode hook including transition function

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+
+export function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const transition = (newMode) => {
+    setMode(newMode)
+  }
+  return { mode, transition };
+}
+
