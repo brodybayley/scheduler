612d013f3681a91bd0aacfc433f8f43ad25fe356
brody bayley <brodybayley@brodys-MacBook-Air.local>
2021-03-22 15:44:35 -0700

    Add for tests for blank and not blank save validation for form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,7 @@ import Button from "components/Button";
 export default function Form(props) {
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   const reset = () => {
     setInterviewer(null);
@@ -16,6 +17,16 @@ export default function Form(props) {
     reset();
     props.onCancel()
   };
+
+  const validate = () => {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+
+    props.onSave(name, interviewer);
+  }
+
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -34,12 +45,13 @@ export default function Form(props) {
           */
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel} >Cancel</Button>
-          <Button confirm onClick={event => props.onSave(name, interviewer)} >Save</Button>
+          <Button confirm onClick={validate} >Save</Button>
         </section>
       </section>
     </main>
