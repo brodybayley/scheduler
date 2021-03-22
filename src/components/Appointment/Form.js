042f0cf1e0ff2a4bfdc9a1fff6d7f589ee061d9c
brody bayley <brodybayley@brodys-MacBook-Air.local>
2021-03-22 14:56:54 -0700

    Add form test file and create first two tests

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -28,6 +28,7 @@ export default function Form(props) {
             placeholder="Enter Student Name"
             value={name}
             onChange={(event) => setName(event.target.value)}
+            data-testid="student-name-input"
           /*
             This must be a controlled component
           */
@@ -44,3 +45,4 @@ export default function Form(props) {
     </main>
   )
 }
+
