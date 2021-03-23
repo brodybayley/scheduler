28df75d1362d98de3aa9694f36493e872a481f11
brody bayley <brodybayley@brodys-MacBook-Air.local>
2021-03-23 15:18:15 -0700

    Complete second test in app js file and add put route to axios js

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -17,10 +17,25 @@ describe("Application", () => {
     });
   });
 
-  it("loads data, books an interview and reduces the spots remaining for the first day by 1", () => {
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
     const { container } = render(<Application />);
+    // use async await to wait until after Archie Cohen renders
+    await waitForElement(() => getByText(container, "Archie Cohen"));
 
-    console.log(prettyDOM(container));
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments[0];
+    // used to start an empty appointment
+    fireEvent.click(getByAltText(appointment, "Add"))
+    // event to input user information
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+    // event to select interviewer
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+    //event to save appointment
+    fireEvent.click(getByText(appointment, "Save"));
+
+    console.log(prettyDOM(appointment));
   });
 
 });
