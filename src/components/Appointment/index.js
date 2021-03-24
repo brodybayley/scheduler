5c265a338bd1d14adbab23fe5cdf8552dea48567
brody bayley <brodybayley@brodys-MacBook-Air.local>
2021-03-23 19:13:46 -0700

    Complete delete test and fix bug in delete error message

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -81,7 +81,7 @@ export default function Appointment(props) {
       )}
       { mode === CONFIRM && (
         <Confirm
-          message={"Delete the appointment?"}
+          message={"Are you sure you would like to delete?"}
           onConfirm={confirmDeletion}
           onCancel={back}
         />
