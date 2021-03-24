b378edea5ee417585e7ee8cd78d80c1fad6d5629
brody bayley <brodybayley@brodys-MacBook-Air.local>
2021-03-24 15:53:11 -0700

    Complete should book an interview test and move command test commands to beforeEach

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,9 +1,16 @@
 describe("Appointments", () => {
-  it("should book an interview", () => {
+  beforeEach(() => {
+    cy.request("GET", "/api/debug/reset");
     cy.visit("/");
     cy.contains("Monday");
+  });
+
+  it("should book an interview", () => {
     cy.get("[alt=Add]").first().click();
     cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
     cy.get("[alt='Sylvia Palmer']").click();
+    cy.contains("Save").click();
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
 });
