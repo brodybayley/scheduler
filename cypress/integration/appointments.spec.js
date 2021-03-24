6ae9de2d4b4cb66c76f03e322a90c2566f0cc618
brody bayley <brodybayley@brodys-MacBook-Air.local>
2021-03-24 15:35:42 -0700

    Add steps to click on blank space add student and add interviewer

---
+++ cypress/integration/appointments.spec.js
@@ -0,0 +1,9 @@
+describe("Appointments", () => {
+  it("should book an interview", () => {
+    cy.visit("/");
+    cy.contains("Monday");
+    cy.get("[alt=Add]").first().click();
+    cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
+    cy.get("[alt='Sylvia Palmer']").click();
+  });
+});
