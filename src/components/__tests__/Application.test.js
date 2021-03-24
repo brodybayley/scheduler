808416c6a545a601e4035bb1ab74bbb90d06769e
brody bayley <brodybayley@brodys-MacBook-Air.local>
2021-03-23 17:29:38 -0700

    Add third function to application test and add sudo code

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -34,16 +34,35 @@ describe("Application", () => {
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
     //event to save appointment
     fireEvent.click(getByText(appointment, "Save"));
-
+    //Check that the element with the text "Saving" is displayed
     expect(getByText(appointment, "Saving")).toBeInTheDocument();
-
+    //Wait until the element with the text "Lydia Miller-Jones" is displayed
     await waitForElement(() => queryByText(appointment, "Lydia Miller-Jones"));
 
     const day = getAllByTestId(container, "day").find(day =>
       queryByText(day, "Monday")
     );
-
+    //check that the DayListItem with the text "Monday" also has the txt "no spots remaining"
     expect(getByText(day, "no spots remaining")).toBeInTheDocument();
   });
+
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+    //1. Render the application
+    const { container } = render(<Application />);
+    //2. Wait until the text "Archie Cohen" is displayed
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    //3. Click the "delete" button on the booked appointment
+
+    //4. Check that the confirmation message is shown.
+
+    // 5. Click the "Confirm" button on the confirmation.
+
+    // 6. Check that the element with the text "Deleting" is displayed.
+
+    // 7. Wait until the element with the "Add" button is displayed.
+
+    // 8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
+
+  })
 
 });
